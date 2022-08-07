ad6cd300e717ec9193021a418ee3f8e55d7be953
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-08-07 14:51:46 +0530

    Create server socket and naming the port

--- src/ServerApp.java
+++ src/ServerApp.java
@@ -1,7 +1,21 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * @author : Gihan Madhusankha
  * 2022-08-06 11:03 PM
  **/
 
 public class ServerApp {
+    public static void main(String[] args) {
+
+        final int PORT = 8000;
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
