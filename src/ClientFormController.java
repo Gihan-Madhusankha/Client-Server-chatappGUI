11891c46edc7db040c9584e8dc9d0e5603b2f664
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-08-07 16:14:11 +0530

    Create a socket

--- src/ClientFormController.java
+++ src/ClientFormController.java
@@ -3,12 +3,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * @author : Gihan Madhusankha
  * 2022-08-06 11:03 PM
  **/
 
 public class ClientFormController {
+
+    final int PORT = 8000;
+    Socket socket;
 
     @FXML
     private TextArea textArea;
@@ -25,7 +32,13 @@ public class ClientFormController {
 
         new Thread(()->{
 
+            try {
+                socket = new Socket("localhost", PORT);
 
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }).start();
     }
